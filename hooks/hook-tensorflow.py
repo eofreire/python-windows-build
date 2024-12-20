5e459f1d03e65cd633caeb1592adb5bd5b2df8c4
eofreire <62474280+eofreire@users.noreply.github.com>
2024-12-20 13:06:13 -0300

    Update hook-tensorflow.py

--- hooks/hook-tensorflow.py
+++ hooks/hook-tensorflow.py
@@ -1,3 +1,8 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
+hiddenimports = collect_submodules('tensorflow')
 datas = collect_data_files('tensorflow')
+
+def pre_safe_import_module(api):
+    # Previne o erro de registro duplo de gradientes
+    api.add_runtime_package('tensorflow.python')
